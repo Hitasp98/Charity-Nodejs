a299103a8aff297f162f5fd99e0c82b39d3eaaeb
Hitap98 <armnoori52@aho.com>
2021-04-29 18:14:39 +0430

    1.4.21

--- Utils/date.js
+++ Utils/date.js
@@ -1,30 +1,60 @@
+
+///تغییر اعداد فارسی به انگلیسی 
+String.prototype.toEnglishDigit = function() {
+  var find = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+  var replace = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var replaceString = this;
+  var regex;
+  for (var i = 0; i < find.length; i++) {
+    regex = new RegExp(find[i], "g");
+    replaceString = replaceString.replace(regex, replace[i]);
+  }
+  return replaceString;
+};
+// var fa_number = "۰۱۲۳۴۵۶۷۸۹"; 
+// console.log(fa_number.toEnglishDigit());
+
 //1398/01/01
 //date1<date2
 function datechack(date1, date2) {
-  let year1 = parseInt(date1.slice(0, 4));
-  let month1 = parseInt(date1.slice(5, 7));
-  let day1 = parseInt(date1.slice(8, 10));
-  ///////////////////////
-  let year2 = parseInt(date2.slice(0, 4));
-  let month2 = parseInt(date2.slice(5, 7));
-  let day2 = parseInt(date2.slice(8, 10));
 
-  //تاریخ شبیه به هم رو اشتباه برمیگردونیم 
-  if (year1 == year2 && month1 == month2 && day1 == day2) {
-    return false;
-  } else if (year1 <= year2) {
-    if (month1 <= month2) {
-      if (day1 <= day2) {
+  let year1 = date1.slice(0, 4)
+  let month1 = date1.slice(5, 7)
+  let day1 = date1.slice(8, 10)
+
+  y1=year1.toEnglishDigit()
+  m1=month1.toEnglishDigit()
+  d1=day1.toEnglishDigit()
+
+  ///////////////////////
+  let year2 = date2.slice(0, 4)
+  let month2 = date2.slice(5, 7)
+  let day2 = date2.slice(8, 10)
+  y2=year2.toEnglishDigit()
+  m2=month2.toEnglishDigit()
+  d2=day2.toEnglishDigit()
+
+  console.log(y1);
+  console.log(y2);
+  //تاریخ شبیه به هم رو اشتباه برمیگردونیم
+  if (year1 == year2) {
+    if (month1 == month2) {
+      if (day1 < day2) {
         return true;
       } else {
         return false;
       }
+    } else if (month1 < month2) {
+      return true;
     } else {
       return false;
     }
+  } else if (year1 < year2) {
+    return true;
   } else {
     return false;
   }
 }
-// console.log(datechack("1400/03/05", "1400/05/07"))
-module.exports={datechack:datechack}
+
+// console.log(datechack("۱۴۰۰/۰۲/۰۴", "۱۴۰۰/۰۲/۰۴"))
+module.exports = { datechack: datechack };
